09ff61fa65ef61573cf3c7ffcca0d61b6413b5c4
Rory Pearson <rorypearson2605@gmail.com>
2025-08-16 18:16:44 +0100

    2 - Added very simple Dockerfile and docker-compose that both seem to work fine

--- front-end/octopus/src/app/api/wakeup/route.ts
+++ front-end/octopus/src/app/api/wakeup/route.ts
@@ -1,5 +1,5 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  req.statusCode = 200;
+export async function GET(request: Request) {
+  return new Response("API is awake!", {
+    status: 200,
+  });
 }
