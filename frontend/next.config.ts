f9c656948c882f50cfdf8a7be0ac893bfcaf2501
Rory Pearson <rorypearson2605@gmail.com>
2025-10-04 20:10:52 +0100

    17 - first attempt at deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NEXT_PUBLIC_APP_ENV === "production";
+const BASE_PATH = isProd ? "/octopus" : undefined;
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "standalone",
+  basePath: BASE_PATH,
 };
 
 export default nextConfig;
