a910f7fe8472da4086ba1a6f17034dd78140d2b1
Rory Pearson <rorypearson2605@gmail.com>
2025-10-11 21:04:33 +0100

    17 - trying rorypearson.co.uk explicitly

--- frontend/app/api/api.ts
+++ frontend/app/api/api.ts
@@ -1,3 +1,6 @@
+import { logger } from '../../pino';
+
 const BACKEND_API = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+logger.info(`Using backend API URL: ${BACKEND_API}`);
 const api = `${BACKEND_API}/api`;
 export default api;
