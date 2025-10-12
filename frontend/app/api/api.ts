de0fec2493c9619ddcef9d46440670874c38bd22
Rory Pearson <rorypearson2605@gmail.com>
2025-10-12 19:13:31 +0100

    17- we ride again

--- frontend/app/api/api.ts
+++ frontend/app/api/api.ts
@@ -2,7 +2,7 @@ import { logger } from '../../pino';
 
 const BACKEND_API = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 const isProd = process.env.NODE_ENV === "production";
-const api = isProd ? "octopus/api" : `${BACKEND_API}/api`;
+const api = isProd ? "/Soctopus/api" : `${BACKEND_API}/api`;
 
 logger.info(`Using backend API URL: ${api}`);
 export default api;
