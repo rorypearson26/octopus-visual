b70b795fb2fbf8359c435c507aee417761a9c690
Rory Pearson <rorypearson2605@gmail.com>
2025-10-14 20:27:04 +0000

    17 - actually seems to be working

--- backend/app/routers/wakeup.py
+++ backend/app/routers/wakeup.py
@@ -7,7 +7,7 @@ router = APIRouter(prefix="/api")
 logger = logging.getLogger(__name__)
 
 
-@router.get("/wakeup/", tags=["setup"])
+@router.get("/wakeup", tags=["setup"])
 async def wakeup():
     logger.info("Waking up backend application")
     return HTTPStatus.OK
